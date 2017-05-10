330c367bb4096ebd99252a43214c3649655fb5ad
interdigitize <interdigitize@users.noreply.github.com>
2017-05-10 09:21:40 -0700

    adds a test that checks if there are duplicate elements in an array and removes them

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,10 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('removes duplicates from a list', () => {
+    const names = ['Groot', 'Peter, aka Starlord', 'Groot', 'Drax', 'Groot', 'Rocket', 'Gamora', 'Groot'];
+    expect(_.uniq(names)).toEqual(['Groot', 'Peter, aka Starlord', 'Drax', 'Rocket', 'Gamora'])
+  });
+
 });
