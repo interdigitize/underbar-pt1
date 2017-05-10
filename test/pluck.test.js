4f8eebaa4cf437623efd526cb98ed9d3f5386796
interdigitize <interdigitize@users.noreply.github.com>
2017-05-10 08:49:24 -0700

    adds a test to pluck that checks that it returns an array of just names as expected

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -11,4 +11,15 @@ describe('pluck()', () => {
     expect(result).toEqual([12, 999, 14]);
   });
 
+  it('returns an array of just names, given an array of people objects', () => {
+    const people = [
+      { name: 'Groot', origin: 'unknown'},
+      { name: 'Starlord', origin: 'an Ego on Earth'},
+      { name: 'Rocket', origin: 'the lab'},
+      { name: 'Drax', origin: 'dont remember'}
+    ];
+    const result = _.pluck(people, 'name');
+    expect(result).toEqual(['Groot', 'Starlord', 'Rocket', 'Drax']);
+  });
+
 });
