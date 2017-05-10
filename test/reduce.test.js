cbba0a14b9d54e8a607821086362b84e51efe751
interdigitize <interdigitize@users.noreply.github.com>
2017-05-10 08:55:46 -0700

    adds two tests to reduce to check that the product of all elements in an array are what is expected. One test starts with an initial accumulator, the other does not.

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -13,4 +13,16 @@ describe('reduce()', () => {
     expect(result).toEqual(15);
   });
 
+  it('reduces an array of numbers to a product, *with* an initial accumulator value', () => {
+    const nums = [1, 2, 3, 4];
+    const result = _.reduce(nums, (acc, num) => acc * num, 1);
+    expect(result).toEqual(24);
+  });
+
+  it('reduces an array of numbers to a product, *without* an initial accumulator value', () => {
+    const nums = [1, 2, 3, 4];
+    const result = _.reduce(nums, (a, b) => a * b);
+    expect(result).toEqual(24);
+  });
+
 });
