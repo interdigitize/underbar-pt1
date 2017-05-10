8c43e860d1d6b2f79b2b6645b35509f10093b673
interdigitize <interdigitize@users.noreply.github.com>
2017-05-10 09:12:40 -0700

    adds two tests that check if the array includes a string value, returns true if it does and false if it does not

--- test/some.test.js
+++ test/some.test.js
@@ -11,4 +11,14 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
+  it('returns true if there is a string value in the array', () => {
+    const arr = [2, 4, 6, 8, 'Groot'];
+    expect(_.some(arr, el => typeof(el) === 'string')).toBe(true);
+  });
+
+  it('returns false if there is not a string value in the array', () => {
+    const arr = [2, 4, 6, 8];
+    expect(_.some(arr, el => typeof(el) === 'string')).toBe(false);
+  });
+
 });
