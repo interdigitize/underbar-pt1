f2f8fdaab3f924b4b2c9b3c65bccf4d5e3cc3706
interdigitize <interdigitize@users.noreply.github.com>
2017-05-10 08:40:39 -0700

    adds a test to map that checks that the sum of two of the element equals what it should

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,11 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+  it('maps every number in array to the sum of two of it', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const mappedArr = _.map(arr, (el) => el + el);
+    expect(mappedArr).toEqual([2, 4, 6, 8, 10]);
+  });
+
 });
